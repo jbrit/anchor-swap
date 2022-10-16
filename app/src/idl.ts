d9387e8fc0a9802c668574b53531c9f627bbdb7b
jibola <pro.ajibolaojo@gmail.com>
2022-10-16 23:56:48 +0100

    app added

---
+++ app/src/idl.ts
@@ -0,0 +1,128 @@
+export type Swap = {
+    "version": "0.1.0",
+    "name": "swap",
+    "instructions": [
+      {
+        "name": "makeSwap",
+        "accounts": [
+          {
+            "name": "jupiterProgram",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "authority",
+            "isMut": true,
+            "isSigner": true
+          },
+          {
+            "name": "swapProgram",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "poolAuthority",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "swapState",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "tokenProgram",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "sourceToken",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "destinationToken",
+            "isMut": false,
+            "isSigner": false
+          }
+        ],
+        "args": [
+          {
+            "name": "inAmount",
+            "type": {
+              "option": "u64"
+            }
+          },
+          {
+            "name": "minimumOutAmount",
+            "type": "u64"
+          }
+        ]
+      }
+    ]
+  };
+  
+  export const IDL: Swap = {
+    "version": "0.1.0",
+    "name": "swap",
+    "instructions": [
+      {
+        "name": "makeSwap",
+        "accounts": [
+          {
+            "name": "jupiterProgram",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "authority",
+            "isMut": true,
+            "isSigner": true
+          },
+          {
+            "name": "swapProgram",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "poolAuthority",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "swapState",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "tokenProgram",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "sourceToken",
+            "isMut": false,
+            "isSigner": false
+          },
+          {
+            "name": "destinationToken",
+            "isMut": false,
+            "isSigner": false
+          }
+        ],
+        "args": [
+          {
+            "name": "inAmount",
+            "type": {
+              "option": "u64"
+            }
+          },
+          {
+            "name": "minimumOutAmount",
+            "type": "u64"
+          }
+        ]
+      }
+    ]
+  };
+  
