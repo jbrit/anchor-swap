2a208a9fd2b555925295c220807d4b7c9a2eece9
jibola <pro.ajibolaojo@gmail.com>
2022-10-17 04:37:24 +0100

    completed a ~

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.css";
-import { useState } from "react";
-import { Connection, PublicKey } from "@solana/web3.js";
-import { Program, AnchorProvider, web3 } from "@project-serum/anchor";
+import {
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  ConfirmOptions,
+} from "@solana/web3.js";
+import { Program, AnchorProvider, BN } from "@project-serum/anchor";
 import idl from "./idl.json";
 
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import {
   useWallet,
+  AnchorWallet,
   WalletProvider,
   ConnectionProvider,
 } from "@solana/wallet-adapter-react";
@@ -14,79 +19,90 @@ import {
   WalletModalProvider,
   WalletMultiButton,
 } from "@solana/wallet-adapter-react-ui";
-import {IDL, Swap} from "./idl"
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { IDL, Swap } from "./idl";
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const wallets = [
   /* view list of available wallets at https://github.com/solana-labs/wallet-adapter#wallets */
   new PhantomWalletAdapter(),
 ];
+const network = clusterApiUrl(WalletAdapterNetwork.Devnet);
 
-const { SystemProgram, Keypair } = web3;
 /* create an account  */
-const baseAccount = Keypair.generate();
 const opts = {
   preflightCommitment: "processed",
-};
+} as ConfirmOptions;
 const programID = new PublicKey(idl.metadata.address);
 
 function App() {
-  const [value, setValue] = useState(null);
-  const wallet = useWallet();
+  const {connected, signTransaction, signAllTransactions, publicKey} = useWallet();
+  const anchorWallet = {
+    signTransaction,
+    signAllTransactions,
+    publicKey,
+  } as AnchorWallet;
 
   async function getProvider() {
     /* create the provider and return it to the caller */
     /* network set to local network for now */
-    const network = "http://127.0.0.1:8899";
-    const connection = new Connection(network, opts.preflightCommitment as "processed");
-    // @ts-ignore
-    const provider = new AnchorProvider(connection, wallet, opts.preflightCommitment as "processed");
+    const connection = new Connection(
+      network,
+      opts.preflightCommitment as "processed"
+    );
+    const provider = new AnchorProvider(connection, anchorWallet, opts);
     return provider;
   }
 
-  async function createCounter() {
+  async function makeSwap() {
     const provider = await getProvider();
     /* create the program interface combining the idl, program ID, and provider */
     const program = new Program<Swap>(IDL, programID, provider);
     try {
       /* interact with the program via rpc */
-      // await program.rpc.makeSwap create({
-      //   accounts: {
-      //     baseAccount: baseAccount.publicKey,
-      //     user: provider.wallet.publicKey,
-      //     systemProgram: SystemProgram.programId,
-      //   },
-      //   signers: [baseAccount],
-      // });
+      console.log("provider.wallet.publicKey: ", anchorWallet.publicKey?.toBase58());
+      const swapTransaction = program.transaction.makeSwap(new BN(0), new BN(0), {
+        accounts: {
+          jupiterProgram: (new PublicKey(
+            "JUP3c2Uh3WA4Ng34tw6kPd2G4C5BB21Xo36Je1s32Ph"
+          )),
+          // Mecurial swap program
+          swapProgram: new PublicKey(
+            "MERLuDFBMmsHnsBPZw2sDQZHvXFMwp8EdjudcU2HKky"
+          ),
+          // Mercurial PAI 3-Pool (USDC-USDT-PAI)
+          swapState: new PublicKey(
+            "SWABtvDnJwWwAb9CbSA3nv7nTnrtYjrACAVtuP3gyBB"
+          ),
+          // PAI 3-pool authority
+          poolAuthority: new PublicKey(
+            "2dc3UgMuVkASzW4sABDjDB5PjFbPTncyECUnZL73bmQR"
+          ),
+          // USDC token
+          sourceToken: new PublicKey(
+            "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"
+          ),
+          // PAI token
+          destinationToken: new PublicKey(
+            "Ea5SjE2Y6yvCeW5dYTn7PYMuW5ikXkvbGdcmSnXeaLjS"
+          ),
+          tokenProgram: new PublicKey(
+            "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+          ),
+          // my account
+          authority: anchorWallet!.publicKey,
+        },
+      });
+     const res = await provider.sendAndConfirm(swapTransaction)
+      // const res = await anchorWallet!.signTransaction!(swapTransaction);
 
-      const account = await program.account
-      // .fetch(
-      //   baseAccount.publicKey
-      // );
-      console.log("account: ", account);
-      // setValue(account.count.toString());
+      console.log("account: ", res);
     } catch (err) {
       console.log("Transaction error: ", err);
     }
   }
 
-  async function increment() {
-    // const provider = await getProvider();
-    // const program = new Program(idl, programID, provider);
-    // await program.rpc.increment({
-    //   accounts: {
-    //     baseAccount: baseAccount.publicKey,
-    //   },
-    // });
-
-    // const account = await program.account.baseAccount.fetch(
-    //   baseAccount.publicKey
-    // );
-    // console.log("account: ", account);
-    // setValue(account.count.toString());
-  }
-
-  if (!wallet.connected) {
+  if (!connected) {
     /* If the user's wallet is not connected, display connect wallet button. */
     return (
       <div
@@ -102,24 +118,14 @@ function App() {
   } else {
     return (
       <div className="App">
-        <div>
-          {!value && <button onClick={createCounter}>Create counter</button>}
-          {value && <button onClick={increment}>Increment counter</button>}
-
-          {value && value >= Number(0) ? (
-            <h2>{value}</h2>
-          ) : (
-            <h3>Please create the counter.</h3>
-          )}
-        </div>
+        {<button onClick={makeSwap}>Swap Or Something</button>}
       </div>
     );
   }
 }
 
-/* wallet configuration as specified here: https://github.com/solana-labs/wallet-adapter#setup */
 const AppWithProvider = () => (
-  <ConnectionProvider endpoint="http://127.0.0.1:8899">
+  <ConnectionProvider endpoint={network}>
     <WalletProvider wallets={wallets} autoConnect>
       <WalletModalProvider>
         <App />
