a6216fcfb5fb7208258a69a0233ffba7c5030328
jibola <pro.ajibolaojo@gmail.com>
2022-10-28 14:13:28 +0100

    working with dynamic remaining accounts

--- programs/swap/src/lib.rs
+++ programs/swap/src/lib.rs
@@ -6,8 +6,8 @@ declare_id!("3uMNrTxKbCN5E55uQAynh2kWAPQReandS3tjxhD85k31");
 #[program]
 pub mod swap {
     use super::*;
-
-    pub fn make_swap(ctx: Context<StartSwap>, in_amount: Option<u64>, minimum_out_amount: u64) -> Result<()> {
+    
+    pub fn make_swap<'info>(ctx: Context<'_, '_, '_, 'info, StartSwap<'info>>, in_amount: Option<u64>, minimum_out_amount: u64) -> Result<()> {
         let cpi_program = ctx.accounts.jupiter_program.to_account_info();
         let cpi_accounts = cpi::accounts::MercurialExchange {
             swap_program: ctx.accounts.swap_program.to_account_info(),
@@ -19,7 +19,7 @@ pub mod swap {
             user_transfer_authority: ctx.accounts.authority.to_account_info(),
         };
         let mut cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
-        cpi_ctx = cpi_ctx.with_remaining_accounts([ctx.accounts.ra1.to_account_info(),ctx.accounts.ra2.to_account_info(),ctx.accounts.ra3.to_account_info()].to_vec());
+        cpi_ctx = cpi_ctx.with_remaining_accounts(ctx.remaining_accounts.to_vec());
         // setting a 0 platfprm_fee_bps
         cpi::mercurial_exchange(cpi_ctx, in_amount, minimum_out_amount, 0)
     }
@@ -45,13 +45,13 @@ pub struct StartSwap<'info> {
     /// CHECK: we don't need to read it in our own program, just the cpi
     #[account(mut)]
     pub destination_token: UncheckedAccount<'info>,
-    /// CHECK: we don't need to read it in our own program, just the cpi
-    #[account(mut)]
-    pub ra1: UncheckedAccount<'info>,
-    /// CHECK: we don't need to read it in our own program, just the cpi
-    #[account(mut)]
-    pub ra2: UncheckedAccount<'info>,
-    /// CHECK: we don't need to read it in our own program, just the cpi
-    #[account(mut)]
-    pub ra3: UncheckedAccount<'info>,
+    // /// CHECK: we don't need to read it in our own program, just the cpi
+    // #[account(mut)]
+    // pub ra1: UncheckedAccount<'info>,
+    // /// CHECK: we don't need to read it in our own program, just the cpi
+    // #[account(mut)]
+    // pub ra2: UncheckedAccount<'info>,
+    // /// CHECK: we don't need to read it in our own program, just the cpi
+    // #[account(mut)]
+    // pub ra3: UncheckedAccount<'info>,
 }
